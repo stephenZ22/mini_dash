aa5110af19b0bb445383182fa73c583c72774ddd
stephenZ22 <stephenzhao94@163.com>
2025-08-10 18:36:05 +0800

    add postgrelsql connect logic

---
+++ pkg/db/db.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/stephenZ22/mini_dash/pkg/logger"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectPostgres(host string, port int, user, password, dbname string) (*gorm.DB, error) {
+	// Build the connection string
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	// Connect to the PostgreSQL database
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	if err != nil {
+		logger.MiniLogger().Error("Failed to connect to database", "error", err)
+		return nil, fmt.Errorf("failed to connect to database: %w", err)
+	}
+
+	sqlDb, err := db.DB()
+	if err != nil {
+		logger.MiniLogger().Error("Failed to get database instance", "error", err)
+		return nil, fmt.Errorf("failed to get database instance: %w", err)
+	}
+
+	sqlDb.SetMaxIdleConns(10)               // Set maximum number of idle connections
+	sqlDb.SetMaxOpenConns(100)              // Set maximum number of open connections
+	sqlDb.SetConnMaxLifetime(1 * time.Hour) // Set maximum connection lifetime (0 means no limit
+
+	return db, nil
+}
